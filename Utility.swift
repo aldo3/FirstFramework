2c8cf2a446db2ad0c6ce535642b7699ef34fd6e5
Al Dockett <al.dockett@gmail.com>
2023-12-10 11:19:45 -0500

    init comit

---
+++ Utility.swift
@@ -0,0 +1,18 @@
+//
+//  Utility.swift
+//  FirstFramework
+//
+//  Created by Al Dockett on 12/10/23.
+//
+
+import Foundation
+
+public class Utility{
+    
+    public static func doSomething(){
+        print("I DID SOMETING......")
+    }
+    
+    
+    
+}
